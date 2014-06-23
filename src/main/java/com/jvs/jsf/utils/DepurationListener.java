f1b768b5210a40c2be1fbc88ed6f13367c6c022e
jorgevs <jevzsz@gmail.com>
2014-06-23 18:43:52 -0500

    DepurationListener added slf4j & log4j added

---
+++ src/main/java/com/jvs/jsf/utils/DepurationListener.java
@@ -0,0 +1,30 @@
+package com.jvs.jsf.utils;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DepurationListener implements PhaseListener {
+
+	private static final Logger log = LoggerFactory.getLogger(DepurationListener.class);
+
+	@Override
+	public void beforePhase(PhaseEvent event) {
+		log.info("BEFORE PHASE:" + event.getPhaseId().toString());
+	}
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+		log.info("AFTER PHASE:" + event.getPhaseId().toString());
+
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.ANY_PHASE;
+	}
+
+}
