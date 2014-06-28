3315b92a40dd01e60f5263a5e0d1707633990164
jorgevs <jevzsz@gmail.com>
2014-06-28 14:34:01 -0500

    Ajax sample to check pwd added

---
+++ src/main/java/com/jvs/jsf/beans/sample/checkpassword/BeanCheckPassword.java
@@ -0,0 +1,42 @@
+package com.jvs.jsf.beans.sample.checkpassword;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "beanCheckPassword")
+@RequestScoped
+public class BeanCheckPassword {
+
+	private String password = "";
+	private String pwdResult = "";
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPwdResult() {
+		if (!getPassword().isEmpty()) {
+			if (getPassword().length() < 3) {
+				pwdResult = "Poor";
+			} else if (getPassword().length() < 5) {
+				pwdResult = "Good";
+			} else if (getPassword().length() < 7) {
+				pwdResult = "Super";
+			} else if (getPassword().length() >= 7) {
+				pwdResult = "Excellent";
+			}
+		} else {
+			pwdResult = "";
+		}
+
+		return pwdResult;
+	}
+
+	public void setPwdResult(String pwdResult) {
+		this.pwdResult = pwdResult;
+	}
+}
